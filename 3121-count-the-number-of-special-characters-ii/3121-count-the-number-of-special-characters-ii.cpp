1e7d80ec5f60417d3349408a7a89169486fcf1a6
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-05-27 23:57:42 +0700

    Time: 69 ms (63.48%), Space: 24.3 MB (68.82%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,30 +1,53 @@
 class Solution {
 public:
+    // int numberOfSpecialChars(string word) {
+    //     unordered_map<char, int> mapp;
+    //     unordered_set<char> lower, upper, valid, invalid;
+
+    //     for (int i = 0; i < word.size(); i++) {
+    //         mapp[word[i]]++;
+    //         if (islower(word[i])) {
+    //             lower.insert(word[i]);
+    //         } else {
+    //             upper.insert(word[i]);
+    //         }
+
+    //         if (mapp.find(tolower(word[i])) != mapp.end() && mapp.find(toupper(word[i])) != mapp.end()) {
+    //             // debug(lower);
+    //             if (isupper(word[i]) && lower.count(tolower(word[i])) && !invalid.count(tolower(word[i]))) {
+    //                 valid.insert(tolower(word[i]));
+    //             }
+
+    //             if (islower(word[i]) && upper.count(toupper(word[i]))) {
+    //                 valid.erase(word[i]);
+    //                 invalid.insert(word[i]);
+    //             }
+    //         }
+    //     }
+
+    //     return valid.size();
+    // }
     int numberOfSpecialChars(string word) {
-        unordered_map<char, int> mapp;
-        unordered_set<char> lower, upper, valid, invalid;
+        vector<int> lower(26, -1); // dr -1, soalny 0 tu valid index
+        vector<int> upper(26, -1);
 
-        for (int i = 0; i < word.size(); i++) {
-            mapp[word[i]]++;
+        for (int i = 0; i < word.size(); i++){
             if (islower(word[i])) {
-                lower.insert(word[i]);
+                lower[word[i] - 'a'] = i;
             } else {
-                upper.insert(word[i]);
-            }
-
-            if (mapp.find(tolower(word[i])) != mapp.end() && mapp.find(toupper(word[i])) != mapp.end()) {
-                // debug(lower);
-                if (isupper(word[i]) && lower.count(tolower(word[i])) && !invalid.count(tolower(word[i]))) {
-                    valid.insert(tolower(word[i]));
-                }
-
-                if (islower(word[i]) && upper.count(toupper(word[i]))) {
-                    valid.erase(word[i]);
-                    invalid.insert(word[i]);
+                if (upper[word[i] - 'A'] == -1){
+                    upper[word[i] - 'A'] = i;
                 }
             }
         }
 
-        return valid.size();
+        int count = 0;
+        for (int i = 0; i < 26; i++){
+            if(lower[i] != -1 && upper[i] != -1 && upper[i] > lower[i]){
+                count++;
+            }
+        }
+
+        return count;
     }
 };
