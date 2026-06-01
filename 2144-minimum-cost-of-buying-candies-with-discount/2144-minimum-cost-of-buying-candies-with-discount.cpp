28e478c14b1091eca4b297549a2699fc7acb0bf0
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-06-01 17:39:30 +0700

    Time: 0 ms (100%), Space: 14.1 MB (77.9%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -4,16 +4,10 @@ public:
         sort(n.begin(), n.end(), greater<int>());
 
         int sum = 0;
-        int jump = 0;
         for (int i = 0; i < n.size(); i++){
-            if (jump == 2) {
-                jump = 0;
-                continue;
+            if (i % 3 != 2) { 
+                sum += n[i];
             }
-            // debug(i);
-            jump++;
-            cout << n[i] << endl;
-            sum += n[i];
         }
 
         return sum;
