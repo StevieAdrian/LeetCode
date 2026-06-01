ae16717179857d75a5664ec294bd0aca75469d2b
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-06-01 19:09:11 +0700

    Time: 3 ms (89.95%), Space: 32.1 MB (56.94%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -3,13 +3,10 @@ public:
     int arrayPairSum(vector<int>& n) {
         sort(n.begin(), n.end());
 
-        int lenp = n.size() / 2;
         int maxx = 0;
 
-        for (int i = 0; i < n.size(); i++) {
-            if (i % 2 == 0){
-                maxx += n[i];
-            }
+        for (int i = 0; i < n.size(); i += 2) {
+            maxx += n[i];
         }
 
         return maxx;
