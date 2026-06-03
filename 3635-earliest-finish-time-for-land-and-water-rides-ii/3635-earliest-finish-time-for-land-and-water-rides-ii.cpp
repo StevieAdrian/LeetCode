0ea2c17ac97fac1990708cf9644589c791222ed2
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-06-03 23:38:38 +0700

    Time: 4 ms (73.39%), Space: 238.2 MB (42.2%) - LeetHub

--- 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3635-earliest-finish-time-for-land-and-water-rides-ii/3635-earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -8,10 +8,7 @@ public:
 
         int land_water = INT_MAX;
         for (int i = 0; i < ws.size(); i++) {
-            land_water = min(
-                land_water,
-                max(ws[i], l) + wd[i]
-            );
+            land_water = min(land_water, max(ws[i], l) + wd[i]);
         }
 
         int w = INT_MAX;
@@ -21,10 +18,7 @@ public:
 
         int water_land = INT_MAX;
         for (int i = 0; i < ls.size(); i++) {
-            water_land = min(
-                water_land,
-                max(ls[i], w) + ld[i]
-            );
+            water_land = min(water_land, max(ls[i], w) + ld[i]);
         }
 
         return min(land_water, water_land);
