27ae4d6a168929acae8e6f611f7a626872ffcb6c
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-06-10 23:48:15 +0700

    Time: 663 ms (59.68%), Space: 327.3 MB (24.19%) - LeetHub

--- 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.cpp
+++ 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     long long maxTotalValue(vector<int>& nums, int k) {
-        int n = nums.size();
-        int logn = 32 - __builtin_clz(n);
-        vector<vector<int>> stMax(n, vector<int>(logn));
-        vector<vector<int>> stMin(n, vector<int>(logn));
-        for (int i = 0; i < n; i++) {
+        int logn = floor(log2(nums.size())) + 1;
+        vector<vector<int>> stMax(nums.size(), vector<int>(logn));
+        vector<vector<int>> stMin(nums.size(), vector<int>(logn));
+
+        for (int i = 0; i < nums.size(); i++) {
             stMax[i][0] = stMin[i][0] = nums[i];
         }
+
         for (int j = 1; j < logn; j++) {
-            for (int i = 0; i + (1 << j) <= n; i++) {
+            for (int i = 0; i + (1 << j) <= nums.size(); i++) {
                 stMax[i][j] =
                     max(stMax[i][j - 1], stMax[i + (1 << (j - 1))][j - 1]);
                 stMin[i][j] =
                     min(stMin[i][j - 1], stMin[i + (1 << (j - 1))][j - 1]);
             }
         }
+
         auto queryMax = [&](int l, int r) {
-            int j = 31 - __builtin_clz(r - l + 1);
+            int j = floor(log2(r - l + 1));
             return max(stMax[l][j], stMax[r - (1 << j) + 1][j]);
         };
+
         auto queryMin = [&](int l, int r) {
-            int j = 31 - __builtin_clz(r - l + 1);
+            int j = floor(log2(r - l + 1));
             return min(stMin[l][j], stMin[r - (1 << j) + 1][j]);
         };
+        
         priority_queue<tuple<int, int, int>> pq;
-        for (int l = 0; l < n; l++) {
-            pq.emplace(queryMax(l, n - 1) - queryMin(l, n - 1), l, n - 1);
+        for (int l = 0; l < nums.size(); l++) {
+            pq.emplace(queryMax(l, nums.size() - 1) - queryMin(l, nums.size() - 1), l, nums.size() - 1);
         }
+
         long long ans = 0;
         while (k--) {
             auto [val, l, r] = pq.top();
@@ -37,6 +42,7 @@ public:
                 pq.emplace(queryMax(l, r - 1) - queryMin(l, r - 1), l, r - 1);
             }
         }
+        
         return ans;
     }
 };
