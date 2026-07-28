31a17a7332583a03d5ab12cbdf07a3435dc25297
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-07-28 22:38:43 +0700

    Time: 155 ms (15.6%), Space: 79.9 MB (16.92%) - LeetHub

---
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        string ans;
+        unordered_map<char, int> ctr;
+
+        for (int i = 0; i < s.size(); i++) {
+            ctr[s[i]]++;
+        }
+        
+        string temp;
+        char temp2 = '\0';
+
+        for (auto p: ctr) {
+            for (int i = 0; i < p.second / 2; i++) {
+                temp += p.first;
+            }
+            if (p.second % 2 == 1){
+                temp2 = p.first;
+            }
+        }
+
+        ans = temp;
+        string temp3 = temp;
+
+        sort(ans.begin(), ans.end());
+        if (temp2 != '\0') {
+            ans += temp2;
+        }
+        sort(temp3.begin(), temp3.end(), greater<int>());
+
+        return ans + temp3;
+    }
+};
