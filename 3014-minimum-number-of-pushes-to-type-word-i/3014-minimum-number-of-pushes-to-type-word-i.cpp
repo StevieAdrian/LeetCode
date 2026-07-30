a42fa3ff66485fc637556bda57ea6d4a6f187f5f
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-07-30 17:31:24 +0700

    Time: 0 ms (100%), Space: 8.4 MB (31.81%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int cnt = 0;
-        int mark = 1;
-        for (int i = 1; i <= word.length(); i++) {
-            cnt += mark;
-            if (i % 8 == 0) {
-                mark++;
-            }
-        }
+        // int cnt = 0;
+        // int mark = 1;
+        // for (int i = 1; i <= word.length(); i++) {
+        //     cnt += mark;
 
-        return cnt;
+        //     if (i % 8 == 0) {
+        //         mark++;
+        //     }
+        // }
+
+        // return cnt;
+        int n = word.length();
+
+        int full = n / 8;
+        int parat = n % 8;
+
+        int ctr = 8 * full * (full + 1) / 2 + parat * (full + 1);
+
+        return ctr;
     }
 };
