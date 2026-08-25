7d8e57e8484ac6abd9c582546ca25ca25f6e85e1
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-08-26 00:40:12 +0700

    Time: 4 ms (30.43%), Space: 8.7 MB (36.05%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || x % 10 == 0 && x != 0){
-            return false;
+        int temp = x;
+        long long int rev = 0;
+
+        while (temp != 0) {
+            rev = rev * 10 + temp % 10;
+            temp /= 10;
         }
 
-        int rev = 0;
-        while (x > rev) {
-            rev = rev * 10 + x % 10;
-            x /= 10;
+        if (rev == x && rev >= 0) {
+            return true;
         }
 
-        return x == rev || x == rev / 10;
+        return false;
     }
 };
