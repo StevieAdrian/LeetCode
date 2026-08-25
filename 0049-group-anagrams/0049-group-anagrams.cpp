4d4996dd182048f8a50c6cec33022aa98680e11e
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-08-26 00:23:29 +0700

    Time: 12 ms (83.35%), Space: 26 MB (51.57%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (int i = 0; i < strs.size(); i++){
+            // string res = sort(strs[i].begin(), strs[i].end());    
+            string res = strs[i];
+            sort(res.begin(), res.end());
+            mp[res].push_back(strs[i]);
+        }
+
+        vector<vector<string>> res;
+
+        for (auto p: mp) {
+            res.push_back(p.second);
+        }
+        
+        return res;
+    }
+};
