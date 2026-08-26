4ae862d686d007ff5cb72800b131a357261f231b
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-08-26 13:29:31 +0700

    Time: 4 ms (34.09%), Space: 20 MB (9.27%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> mpp;
-        vector<int> ans;
-
+        unordered_map<int, int> mp;
         for (int i = 0; i < nums.size(); i++) {
-            mpp[nums[i]]++;
-        }
-        
-        vector<pair<int, int>> temp(mpp.begin(), mpp.end());
-        sort(temp.begin(), temp.end(), [](auto a, auto b) {
-            return a.second > b.second;
-        });
-
-        for (int i = 0; i < k; i++){
-            ans.push_back(temp[i].first);
+            mp[nums[i]]++;
         }
 
-        return ans;
+        vector<vector<int>> bucket(nums.size()+1);
+
+        for (auto p: mp) {
+            bucket[p.second].push_back(p.first);
+        }
+
+        vector<int> result;
+        for (int i = bucket.size() - 1; i >= 0 && k > 0; i--) {
+            for (auto num: bucket[i]) {
+                result.push_back(num);
+                k--;
+                if (k == 0) break;
+            }
+        }
+        /*
+            1, 1, 1, 1, 2, 3, 3
+            bucket[4] = 1
+            bucket[1] = 2
+            bucket[2] = 3
+            
+            print from biggest bucket size
+        */
+        return result;
     }
 };
