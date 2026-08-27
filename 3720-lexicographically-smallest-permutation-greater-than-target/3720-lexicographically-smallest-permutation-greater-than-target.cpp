a6e0dde80725e6494c10e42857e9c736aaa8a7a1
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-08-27 22:14:41 +0700

    Time: 7 ms (42.04%), Space: 9.9 MB (82.17%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    bool permutate(string& s, string& curr, vector<bool>& used,
+                   string& target, string& ans, int cmp) {
+
+        if (curr.length() == s.length()) {
+            if (cmp > 0) {
+                ans = curr;
+                return true;
+            }
+            return false;
+        }
+
+        int pos = curr.length();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (used[i]) continue;
+
+            if (i > 0 && s[i] == s[i - 1] && !used[i - 1])
+                continue;
+
+            int newCmp = cmp;
+
+            if (cmp == 0) {
+                if (s[i] < target[pos])
+                    newCmp = -1;
+                else if (s[i] > target[pos])
+                    newCmp = 1;
+            }
+
+            // Kalau sudah lebih kecil, branch ini pasti gagal
+            if (newCmp < 0)
+                continue;
+
+            used[i] = true;
+            curr += s[i];
+
+            if (permutate(s, curr, used, target, ans, newCmp))
+                return true;
+
+            curr.pop_back();
+            used[i] = false;
+        }
+
+        return false;
+    }
+
+    string lexGreaterPermutation(string s, string target) {
+        sort(s.begin(), s.end());
+
+        vector<bool> used(s.length(), false);
+        string curr = "";
+        string ans = "";
+
+        permutate(s, curr, used, target, ans, 0);
+
+        return ans;
+    }
+};
