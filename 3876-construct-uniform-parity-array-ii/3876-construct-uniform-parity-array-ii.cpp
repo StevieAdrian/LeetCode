734ece3dac7dab79735dd25fb313b3373aa74236
Stevie Adrian <134636973+StevieAdrian@users.noreply.github.com>
2026-09-03 17:47:51 +0700

    Time: 0 ms (100%), Space: 165.7 MB (99.48%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int minn = nums[0];
+        int minn = *min_element(nums.begin(), nums.end());
 
-        for (int i = 1; i < nums.size(); i++){
+        for (int i = 0; i < nums.size(); i++){
             if ((nums[i] % 2) != minn % 2 && (nums[i] - minn) % 2 != minn % 2) {
                 return false;
             }
